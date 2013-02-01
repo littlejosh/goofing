c755e83e9f826c154b464a0bb8bbdf976f3a9d28
littlejosh <joshuarlittle@gmail.com>
2013-01-31 21:29:15 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goofing::Application.config.secret_token = '08695e71289c094d5079183222db796de3c427ecbc9dfb8b753efcf498acd04e8ee1ef41e08953828e83e529d9e44b7234aeb68e59059e4cf68b2e7a40cffd9f'
